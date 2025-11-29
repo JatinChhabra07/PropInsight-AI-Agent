2a25ce9e2506c98b84d9b70064cddc8b3dcf6423
JatinChhabra07 <chhabrajatin0073@gmail.com>
2025-11-29 20:00:17 +0530

    Add financial calculator tool with pandas

---
+++ app/tools.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+
+def calculate_rent_rolls(path: str):
+    """
+    Reads an Excel rent roll and calculates total income and occupancy.
+    """
+    try:
+
+        dataset = pd.read_excel(r"C:\Users\chhab\PropInsight-AI-Agent\data\rent_roll (1).xlsx")
+        if 'Monthly_Rent' not in dataset.columns:
+            raise ValueError("column Monthly_Rent is not found in the dataset!")
+        
+        total_income = dataset['Monthly_Rent'].sum()
+        
+        if ' Status' in dataset.columns:
+            total_units = len(dataset)
+            occupied_units = len(dataset[dataset[' Status']=='Occupied'])
+            occupancy_rate = (occupied_units / total_units) * 100
+        else:
+            occupancy_rate = 100
+
+        return (
+            f"Financial Analysis:\n"
+            f"- Total Monthly Income: ${total_income:,.2f}\n"
+            f"- Occupancy Rate: {occupancy_rate:.1f}%"
+        )
+    except Exception as e:
+        return f"Error reading file: {str(e)}"
+
+
+
+
+        
+    
+
+    
---
+++ test_tool.py
@@ -0,0 +1,9 @@
+from app.tools import calculate_rent_rolls
+
+file_path = "data/rent_roll.xlsx"
+
+# Run the function
+result = calculate_rent_rolls(file_path)
+
+# Print the output
+print(result)
